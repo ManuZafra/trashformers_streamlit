fe75755962c2d937423a8572d149b722a405479a
Manuel Zafra <mzafraox@gmail.com>
2024-12-29 18:31:10 +0100

    Renombrar archivo principal a main.py para Streamlit

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import cv2
+
+st.title("Test de OpenCV en Streamlit Cloud")
+st.write(f"Versión de OpenCV: {cv2.__version__}")
+                                                
